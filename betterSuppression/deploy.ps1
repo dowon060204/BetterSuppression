3dae4c30681e0b5cfc68294721c434d8ddeae178
pensil020406 <dowon060204@gmail.com>
2026-08-11 21:56:39 +0900

    Delete betterSuppression directory

--- betterSuppression/deploy.ps1
+++
@@ -1,9 +0,0 @@
-$p = Get-Process "Lethal Company" -ErrorAction SilentlyContinue
-while ($p -ne $null) {
-    Start-Sleep -Seconds 2
-    $p = Get-Process "Lethal Company" -ErrorAction SilentlyContinue
-}
-Start-Sleep -Seconds 1
-Copy-Item "C:\Users\kmh04\Desktop\Folder\ai\LethalCompany\betterSuppression\BetterSuppression.dll" "C:\Users\kmh04\AppData\Roaming\com.kesomannen.gale\lethal-company\profiles\Default\BepInEx\plugins\BetterSuppression\BetterSuppression.dll" -Force
-Remove-Item "C:\Users\kmh04\AppData\Roaming\com.kesomannen.gale\lethal-company\profiles\Default\BepInEx\config\com.lethalcompany.bettersuppression.cfg" -Force -ErrorAction SilentlyContinue
-Write-Host "Deploy complete!"
